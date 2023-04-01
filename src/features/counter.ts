77f19d8580666759ea1fcdb4d08960044d6da202
Ashik <ashik@pop-os.localdomain>
2023-04-01 20:17:34 +0600

    redux installl and start

---
+++ src/features/counter.ts
@@ -0,0 +1,22 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const counterSlice = createSlice({
+  name: "counter",
+  initialState: {
+    value: 0,
+  },
+  reducers: {
+    increment: (state) => {
+      state.value += 1;
+    },
+    decrement: (state) => {
+      state.value -= 1;
+    },
+  },
+});
+
+export const { increment } = counterSlice.actions;
+
+const counterReducer = counterSlice.reducer;
+
+export default counterReducer;
